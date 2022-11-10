eb3daf2844df763364dd11ac570422fb5f91fbc5
JhonatasHellw <115406423+JhonatasHellw@users.noreply.github.com>
2022-11-10 18:24:44 -0300

    Jhonatas/ Modificações e adição do formcadatro no componente

--- scarproject/src/App.js
+++ scarproject/src/App.js
@@ -8,15 +8,18 @@ from "react-router-dom";
 import Home from "./components/pages/Home"
 import FormCadastro from "./components/pages/FormCadastro"
 import FormLogin from "./components/pages/FormLogin"
+import "./App.css"
 
 function App() {
   return (
     <Router>
 
-      <div>
-          <Link to ="/">Home</Link>
-          <Link to ="/cadastro">Cadastrar</Link>
-          <Link to ="/login">Login</Link>
+      <div id="menu">
+        <ul>
+          <li><Link to ="/">Home</Link></li>
+          <li><Link to ="/cadastro">Cadastrar</Link></li>
+          <li><Link to ="/login">Login</Link></li>
+        </ul>
       </div>
 
       <Switch>
--- scarproject/src/components/pages/FormCadastro.js
+++ scarproject/src/components/pages/FormCadastro.js
@@ -1,7 +1,69 @@
-function FormCadastro(){
+import React, { Component } from "react";
+import './FormCadastro.css';
 
-    return <h1>Cadastrar</h1>
+class FormCadastro extends Component {
 
+    constructor(props) {
+
+        super(props)
+
+        this.state = {
+
+            nome_completo: '',
+            email: '',
+            senha: ''
+
+        }
+
+    }
+    render() {
+
+        // const {email} = this.state;
+        // const {senha} = this.state
+
+        const handleSubmit = () => {
+
+            //e.preventDefault();
+
+        };
+
+
+        return (
+            <main>
+                <div>
+                <h1>SCAR</h1>git
+                    <p id='P_cabecalho'>Faça seu Cadastro!</p>
+                    <h2>Cadastre-se</h2>
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <p>Nome completo:</p>
+                        <input name="nome_completo" type="text" placeholder="Nome completo" />
+                    </label>
+                    <label>
+                        <p>Email:</p>
+                        <input name="email" type='text' placeholder="email@escolar.ifrn.edu.br" />
+                    </label>
+                    <label>
+                        <p>Senha:</p>
+                        <input name="senha" type='password' placeholder="***********" />
+                    </label>
+                    <label>
+                        <p>Curso:</p>
+                        <input name="Curso" type="text" placeholder="Insira seu curso" />
+                    </label>
+                    <label>
+                        <p>Matricula:</p>
+                        <input name="matricula" type="text" placeholder="Número de Matrícula" />
+                    </label>
+                    <button type="submit">Cadastrar</button>
+
+                </form>
+                <a>Já possui uma conta? Logue já!</a>
+            </main>
+        )
+    }
 }
 
-export default FormCadastro;
+export default FormCadastro
