5fbf25dc66e9b5eb1818d3de5c3ef26274fd9ba3
JhonatasHellw <jhonatasanthony6@gmail.com>
2022-11-13 17:08:05 -0300

    Mudança no nome do swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,16 +4,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
+    .setTitle('Scar')
+    .setDescription('The Scar API description')
     .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
